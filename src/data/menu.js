5944de9befbd1f576446b014b175c965a6453174
Felipe Cerero <lfcerero@misena.edu.co>
2019-07-02 13:44:35 -0500

    suebiendo proyecto

---
+++ src/data/menu.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: 'Home',
+    url: '/'
+  },
+  {
+    title: 'Mis productos',
+    url: '/about'
+  },
+  {
+    title: 'Contacto',
+    url: '/contact'
+  },
+  {
+    title: 'Chat',
+    url: '/library'
+  }
+];
